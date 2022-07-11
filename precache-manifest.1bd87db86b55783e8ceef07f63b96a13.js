ee41f4cf9ba9e5f87519f9b8331e3c5680b66157
Akshat Singh <raghuvanshiakshatsingh@gmail.com>
2022-07-12 03:15:28 +0530

    Add files via upload

---
+++ precache-manifest.1bd87db86b55783e8ceef07f63b96a13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2af66feddb704d8be015",
+    "url": "/static/css/main.dee863c2.chunk.css"
+  },
+  {
+    "revision": "2af66feddb704d8be015",
+    "url": "/static/js/main.2af66fed.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "4e5293350b3113a27c03",
+    "url": "/static/css/2.a7b34cc8.chunk.css"
+  },
+  {
+    "revision": "4e5293350b3113a27c03",
+    "url": "/static/js/2.4e529335.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "54dfc5ac080e4486912f95e68bb6f91c",
+    "url": "/index.html"
+  }
+];
